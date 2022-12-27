316175d4fe35111b075db862d85768315de63b63
krishnaSTL <115693638+krishnaSTL@users.noreply.github.com>
2022-12-27 11:16:15 +0530

    Add files via upload

---
+++ pdf.py
@@ -0,0 +1,13 @@
+from fpdf import FPDF
+import os
+pdf=FPDF()
+pdf.set_auto_page_break(0)
+img_list=[x for x in os.listdir("updated")]
+
+for img in img_list:
+    pdf.add_page()
+    image="updated\\"+img
+    pdf.image(image,w=200,h=150)
+
+pdf.output("final.pdf")
+print("success")
